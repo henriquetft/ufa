4299c3f02eee700267b6496e4f0f2bb2eef1c212
Henrique Teofilo <henrique@teofilo.me>
2021-05-17 19:52:05 -0300

    Improving nautilus extension

--- extensions/nautilus/ufa.py
+++ extensions/nautilus/ufa.py
@@ -32,12 +32,18 @@
 """
 UFA Nautilus integration.
 
+Creates context menus helping user to manage tags.
+
 Creates a context menu on right-click of files within a repository
-that opens a dialog allowing the user to set and unset tags
+that opens a dialog allowing the user to set and unset tags.
+
+A context menu on right-click of a repository folder allowing user
+to create a tag.
 """
 
 import subprocess
 import shutil
+import os.path
 from urllib.parse import unquote
 from subprocess import Popen, TimeoutExpired
 from gi.repository import Nautilus, GObject
@@ -55,6 +61,13 @@ def get_ufa_repo_dir(filepath):
         return None
 
 
+def is_repo_dir(filepath):
+    """ Returns whether the filepath is a repository path """
+    filepath += "/.ufarepo"
+    log(f"checking file: {filepath}")
+    return os.path.isfile(filepath)
+
+
 def log(message):
     """ Logs message to the console """
     print(message)
@@ -70,12 +83,14 @@ class UFACommand:
     @classmethod
     def execute(cls, s, exp=[0]):
         """ Executes a shell command """
+        command = s.split(" ")[0]
         with Popen(s, shell=True, stdout=subprocess.PIPE) as proc:
             try:
                 log(f'\nExecuting command:\n{s}')
                 stdout = proc.communicate()[0]
                 if proc.returncode not in exp:
-                    raise Exception("retcode {}".format(proc.returncode))
+                    raise Exception("Error on {}. return code: {}".format(
+                        command, proc.returncode))
                 stdout = stdout.decode('utf-8')
                 log(f'\nCommand return stdout:\n{stdout}')
                 return stdout
@@ -116,6 +131,11 @@ class UFACommand:
                                                 filename,
                                                 tag))
 
+    def create_tag(self, tagname):
+        self.execute("{} -r {} create {}".format(self.ufatag,
+                                                 self.repository,
+                                                 tagname))
+
 
 class Command(UFACommand):
     def dialog_tags(self, filename, all_tags, tags_for_file):
@@ -137,13 +157,22 @@ class Command(UFACommand):
             return []
         return tags_checked
 
+    def dialog_new_tag(self):
+        stdout = self.execute("""zenity --entry --title='Add new tag' \
+            --text='Enter name of new tag:'""", exp=[0, 1])
+        return stdout
+
+    def dialog_info(self, message):
+        self.execute(f"""zenity --info --title='UFA' --width=200 \
+                        --text='{message}'""")
+
 
 class MenuProvider(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
         self.ufatag_app = shutil.which("ufatag")
 
-    def menu_activate_tags(self, menu, ufarepo, filepath):
-        log(f"Calling activate method: repo={ufarepo}, filepath={filepath}")
+    def menu_activate_manage_tags(self, menu, ufarepo, filepath):
+        log(f"Calling manage_tags: repo={ufarepo}, filepath={filepath}")
         filename = filepath.split('/')[-1:][0]
         log(f"Filename: {filename}")
 
@@ -161,6 +190,20 @@ class MenuProvider(GObject.GObject, Nautilus.MenuProvider):
         log("Executing sets and unsets commands ...")
         cmd.set_tags_for_file(filename, tags_to_set)
         cmd.unset_tags_for_file(filename, tags_to_unset)
+
+    def menu_activate_create_tags(self, menu, ufarepo):
+        log(f"Calling create_tags: repo={ufarepo}")
+        cmd = Command(self.ufatag_app, ufarepo)
+        tag_name = cmd.dialog_new_tag()
+        tag_name = tag_name.strip().replace(" ", "-")
+        if tag_name:
+            log(f"Checking whether a tag exists: {tag_name}")
+            all_tags = cmd.get_all_tags()
+            if tag_name in all_tags:
+                cmd.dialog_info(f"Tag {tag_name} already exist")
+            else:
+                log(f"Creating tag: {tag_name}")
+                cmd.create_tag(tag_name)
 
     def get_file_items(self, window, files):
         if self.ufatag_app is None:
@@ -198,10 +241,41 @@ class MenuProvider(GObject.GObject, Nautilus.MenuProvider):
         submenu.append_item(sub_menuitem)
 
         sub_menuitem.connect('activate',
-                             self.menu_activate_tags,
+                             self.menu_activate_manage_tags,
                              ufarepo,
                              filepath)
         return (top_menuitem,)
 
     def get_background_items(self, window, file):
-        return None
+        if self.ufatag_app is None:
+            log("ufatag not found")
+            return None
+
+        if not file.is_directory() or file.get_uri_scheme() != 'file':
+            return None
+
+        log("\n------------------------------------------------------------\n")
+        filepath = str(file.get_uri())
+        filepath = filepath.split("file://")[1]
+        filepath = unquote(filepath)
+        if not is_repo_dir(filepath):
+            return None
+
+        submenu = Nautilus.Menu()
+        submenu_item = Nautilus.MenuItem(name='MenuProvider::Create_Tag',
+                                              label='Create tag',
+                                              tip='',
+                                              icon='')
+        submenu.append_item(submenu_item)
+
+        menuitem = Nautilus.MenuItem(name='MenuProvider::UFA_Options',
+                                     label='UFA',
+                                     tip='',
+                                     icon='')
+        menuitem.set_submenu(submenu)
+
+        submenu_item.connect('activate',
+                             self.menu_activate_create_tags,
+                             filepath)
+
+        return (menuitem,)
