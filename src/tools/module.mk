f9312a7549a582abbe21fa0e9b062c4a64b5cbae
henriquetft <henrique@teofilo.me>
2022-06-23 10:12:56 -0300

    find command line utility

--- src/tools/module.mk
+++ src/tools/module.mk
@@ -13,9 +13,12 @@ PROGRAMS += ufafind ufaset ufatag
 
 	
 ufafind : $(LOCAL_DIR)/ufafind.o \
-		src/util/logging.o \
-		src/util/misc.o \
-		src/util/list.o
+	src/util/logging.o \
+	src/util/misc.o \
+	src/util/list.o \
+	src/core/repo_sqlite.o \
+	src/util/error.o \
+	-lsqlite3
 	@echo creating "$@" executable...
 	$(LD) $(CFLAGS) -o"$@" $^ $(LDFLAGS)
 
